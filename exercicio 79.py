67599e3f47c43fff2b742108095ba7902c95749f
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-23 15:34:45 -0400

    doc: att

---
+++ exercicio 79.py
@@ -0,0 +1,16 @@
+lista_num = []
+while True:
+    num = int(input('Digite um valor: '))
+    if num in lista_num:
+        print('Numero ja existe na lista, não da pra adicionar na lista')
+    else:
+        lista_num.append(num)
+        print('Numero adicionado com sucesso!')
+        
+    continuar = str(input('Deseja continuar? [S/N]: '))
+    if continuar.lower() == 's':
+        continue
+    else:
+        break
+
+print(sorted(lista_num))
