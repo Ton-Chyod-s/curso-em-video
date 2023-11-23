3585bbd1784dab7e24ba697182d4712742eabbd1
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-23 08:51:06 -0400

    doc: att

--- exercicio 74.py
+++ exercicio 74.py
@@ -4,7 +4,7 @@ from random import randint
 lista_num = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
 
 print('-=' * 15)
-print(f'A lista é: {lista_num}')
+print(f'A lista é: {lista_num}'.replace('(','').replace(')',''))
 print('-=' * 15)
 print(f'O maior é: {max(lista_num)}')
 print('-=' * 15)
