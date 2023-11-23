a422de8b95078d1bd7d88eab0566ba1b61c94fc7
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-23 08:47:27 -0400

    doc: att

---
+++ exercicio 74.py
@@ -0,0 +1,12 @@
+from random import randint
+
+
+lista_num = (randint(1,10), randint(1,10), randint(1,10), randint(1,10), randint(1,10))
+
+print('-=' * 15)
+print(f'A lista é: {lista_num}')
+print('-=' * 15)
+print(f'O maior é: {max(lista_num)}')
+print('-=' * 15)
+print(f'O menor é: {min(lista_num)}')
+print('-=' * 15)
