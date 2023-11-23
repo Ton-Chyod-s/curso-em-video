e1a5ab53c6476e62cc1524d9e35bd509d06b05d5
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-23 10:14:45 -0400

    doc: att

--- exercicio 75.py
+++ exercicio 75.py
@@ -5,26 +5,29 @@ num4 = int(input('Digite o quarto número: '))
 
 lista_tup = (num1,num2,num3,num4)
 
+print(f'Você digitou os valores {lista_tup}')
+
 #A) Quantas vezes apareceu o valor 9.
 apareceu = 0
 for i in lista_tup:
-    if i == 9:
+    if 9 in lista_tup:
         apareceu += 1
 
 if apareceu == 0:
     print('Não foi econtrado o número 9')
 else:
-    print(f'O numero 9 apareceu: {apareceu}')
+    print(f'O valor 9 apareceu: {apareceu} vezes')
     
 #B) Em que posição foi digitado o primeiro valor 3.
-try:
-    print(f'o numero 3 foi encontrado na posicão: {lista_tup.index(3)}')
-except:
+if 3 in lista_tup:
+    print(f'O valor 3 apareceu na {lista_tup.index(3)+1}º posição')
+else:
     print('Não foi econtrado o número 3')
 
 #C) Quais foram os números pares.
+print(f'Os valores pares foram ', end = '')
 for i in lista_tup:
     if i % 2 == 0:
-        print(f'Número par: {i}')
+        print(i, end = ' ')
 
     
