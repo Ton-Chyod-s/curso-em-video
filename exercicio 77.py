50b6aab3c1e9dfda8bfbebab558ce34b34d9bb7e
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-23 11:19:27 -0400

    doc: att

--- exercicio 77.py
+++ exercicio 77.py
@@ -3,3 +3,11 @@ palavras = (
     'TRABALHAR','MERCADO','PROGRAMAR','FUTURO'
 )
 
+vogais = ('A','E','I','O','U')
+
+for palavra in palavras:
+    print(f'\nNa palavra {palavra} temos', end=' ')
+    for letra in palavra:
+        if letra in vogais:
+           print(letra.lower(), end=' ')
+    
