5cb1408ac3490eb711c16c220328c711c8fcb3eb
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-23 08:56:01 -0400

    doc: att

---
+++ exercicio 75.py
@@ -0,0 +1,12 @@
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+num3 = int(input('Digite o terceiro número: '))
+num4 = int(input('Digite o quarto número: '))
+
+lista_tup = (num1,num2,num3,num4)
+
+#A) Quantas vezes apareceu o valor 9.
+
+#B) Em que posição foi digitado o primeiro valor 3.
+
+#C) Quais foram os números pares.
