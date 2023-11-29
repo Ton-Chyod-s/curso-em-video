6a3966f2bd688a2407394a5a4b47d39cfceb4443
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-29 16:43:54 -0400

    doc: att

--- exercicio 94.py
+++ exercicio 94.py
@@ -3,53 +3,40 @@ pessoa = {}
 soma_idade = 0
 mulheres = []
 acima_idade = []
-
-
-pessoa['Nome'] = str(input('Nome: '))
 while True:
-    pessoa['Sexo'] = str(input('Sexo: [M/F]'.upper()[0]))
-    if pessoa['Sexo'] in 'M/F':
+    pessoa['Nome'] = str(input('Nome: '))
+    while True:
+        pessoa['Sexo'] = str(input('Sexo [M/F]: ')).upper()
+        if pessoa['Sexo'] in 'M/F':
+            break
+        print('ERRO! Por favor, digite apenas M ou F.')
+    pessoa['Idade'] = int(input('Idade: '))
+    soma_idade += pessoa['Idade']
+    formulario.append(pessoa.copy())
+    while True:
+        continuar = str(input('Deseja continuar? [S/N] ')).upper()
+        if continuar in 'SN':
+            break
+        print('ERRO! Responda apenas S ou N.')
+    if continuar == 'N':
         break
-    print('ERRO! Por favor, digite apenas M ou F.')
-
-pessoa['Idade'] = int(input('Idade: '))
-
-formulario.append(pessoa.copy())
-while True:
-    continuar = str(input('Deseja continuar? [S/N]'))
-    if continuar in 'Nn':
-        break
-    print('ERRO! Responda apenas S ou N.')
-
-    if continuar == 'S':
-        break
-
-print(f'Pessoas cadastradas {len(formulario)}')
-
-for num, valor in enumerate(formulario):
-    idade = formulario[num]['Idade']
-    soma_idade += idade
-
-print(f'A média das idades é {soma_idade / len(formulario)}')
-
+print('-=' * 20)
+print(f'Ao total temos {len(formulario)} pessoas cadastradas.')
+print(f'A média das idades é de {soma_idade / len(formulario)}')
 for num, valor in enumerate(formulario):
     sexo = formulario[num]['Sexo']
     if sexo in 'Ff':
         mulheres.append(formulario[num]['Nome'])
-
 print(f'As mulheres cadastradas foram ',end=' ')
 for valor in mulheres:
     print(valor, end=',')
 print()
-
 for num, valor in enumerate(formulario):
     if formulario[num]['Idade'] > soma_idade / len(formulario):
         acima_idade.append(valor)
-
 print(f'Lista das pessoas que estão acima da média')
 for valor in acima_idade:
     for num, valor in valor.items():
         print(f'{num} = {valor}', end='; ')
     print()
-
 print('FINALIZADO!!')   
