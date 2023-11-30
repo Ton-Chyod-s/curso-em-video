e92bf534f7be89b8f3c07ed9d3f854ecd2bfe41e
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-11-30 11:07:46 -0400

    doc: att

--- exercicio 96.py
+++ exercicio 96.py
@@ -2,13 +2,23 @@ def área(larg, comp):
     res = larg*comp
     return print(f'A área de um terreno {larg}x{comp} é de {res}m².')
 
+
 def titulo(txt):
     print(txt)
     print('-' * 22)
 
 
-titulo('Controle de Terrenos')
-largura = float(input('LARGURA (m): '))
-comprimento = float(input('COMPRIMENTO (m): '))
-área(largura,comprimento)
-print()
+while True:
+    titulo('Controle de Terrenos')
+    largura = float(input('LARGURA (m): '))
+    comprimento = float(input('COMPRIMENTO (m): '))
+    área(largura,comprimento)
+    print()
+    while True:
+        cancelar = str(input('Deseja cancelar? [S/N]')).upper()[0]
+        if cancelar in 'SN':
+            print('-- PROGRAMA FINALIZADO!! --')
+            break
+        print('ERRO! Digite apenas S e N.')
+    if cancelar == 'N':
+        break
---
+++ exercicio 97.py
@@ -0,0 +1,10 @@
+def escreva(txt):
+    tamanho = len(txt) + 4
+    print('~' * tamanho )
+    print(f"  {txt}")
+    print('~' * tamanho )
+
+   
+escreva('Klayton Dias')
+escreva('Curso de python no youtube')
+escreva('Cev')
