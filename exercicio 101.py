b785c796d50442792eacd5be2ce1121d1620c799
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-12-01 08:19:57 -0400

    doc: att

--- exercicio 101.py
+++ exercicio 101.py
@@ -1,6 +1,14 @@
 def voto(ano_nascimento):
-    res = ''
-    return res
+    from datetime import date
+    data_atual = date.today()
+    if data_atual.year - ano_nascimento == 1 or data_atual.year - ano_nascimento < 18:
+        res = 'VOTO NEGADO'
+    elif data_atual.year - ano_nascimento >= 18 and data_atual.year - ano_nascimento < 65:
+        res =  'VOTO OBRIGATÓRIO'
+    else:
+        res = 'VOTO OPCIONAL'
+    return F'Com {data_atual.year - ano_nascimento} anos: {res}'
 
-ano = int(input('Digite o ano de nascimento: '))
-voto(ano)
+print('-=' * 20)
+ano = int(input('Em que ano você nasceu? '))
+print(voto(ano))
