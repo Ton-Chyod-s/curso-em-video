fdb97cdd43219968efbb29a6a90db431aa54372e
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-12-01 15:27:33 -0400

    doc: att

---
+++ exercicio 108.py
@@ -0,0 +1,7 @@
+from ex108 import moeda
+
+num= float(input('Digite um preço: R$'))
+print(f'Aumentando 10%, temos {moeda.moeda(moeda.aumentar(num,10))}')
+print(f'Diminuindo 30%, temos {moeda.moeda(moeda.diminuir(num,30))}')
+print(f'A dobro de {moeda.moeda(num)} é {moeda.moeda(moeda.dobro(num))}')
+print(f'A metade de {moeda.moeda(num)} é {moeda.moeda(moeda.metade(num))}')
