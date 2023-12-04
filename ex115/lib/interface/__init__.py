9e1437debc626d8aff537a52296c9a28fe198ad5
Klay <65176457+Ton-Chyod-s@users.noreply.github.com>
2023-12-04 17:27:25 -0400

    doc: att

--- ex115/lib/interface/__init__.py
+++ ex115/lib/interface/__init__.py
@@ -2,10 +2,29 @@ def linha(tamanho = 42):
     return '-' * tamanho
 
 
+def leiaInt(txt):
+    while True:
+        num = str(input(txt))
+        if num.isnumeric():
+            return int(num)
+        else:
+            print(f"\033[0;31mERRO! Digite um número inteiro válido.\033[m")
+
+
 def cabecalho(txt):
     print(linha())
     print(txt.center(42))
     print(linha())
+
+
+def menu(*args):
+    list_op = args
+    cabecalho('MENU PRINCIPAL')
+    for num, valor in enumerate(list_op):
+        print(f'\033[034m{num+1} - {valor}\033[m')
+    print(linha())
+    num_op = leiaInt(f"\033[033mSua Opção: \033[m")
+    return num_op
 
 
 def criar(txt,arquivo):
@@ -25,7 +44,5 @@ def adicionando(txt,arquivo):
 
 
 if __name__ == '__main__':
-    #criar('Klayton','pessoas')
-    adicionando('i','pessoas')
-    #arquivo = ler('pessoas').split(',')
+    menu('Ver pessoas cadastradas','Cadastrar nova Pessoa','Sair do sistema')
     
--- exercicio 115a.py
+++ exercicio 115a.py
@@ -1,3 +1,15 @@
-from ex115 import *
+from ex115.lib.interface import *
+from time import sleep
 
-menu(1,2)
+while True:
+    numero = menu('Ver pessoas cadastradas','Cadastrar nova Pessoa','Sair do sistema')
+    if numero == 3:
+        cabecalho('Saindo do sistema... Até logo!')
+        break
+    elif numero > 3 or numero < 0:
+        print(f'\033[031mDigite uma opção válida\033[m')
+    elif numero == 1:
+        cabecalho(f'Opção 1')
+    else:  
+        cabecalho(f'Opção 2')
+    sleep(.2)
