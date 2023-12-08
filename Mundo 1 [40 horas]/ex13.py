4bc8a5ee875d7d0ff6cba4970ae3e1f6c83731f1
Klay <arqkdias@gmail.com>
2023-12-08 14:04:44 -0400

    doc: att

---
+++ Mundo 1 [40 horas]/ex13.py
@@ -0,0 +1,3 @@
+salario = float(input('Digite seu salario: R$'))
+aumento = salario * 0.15
+print(f'Seu salario atual é de R${salario}, seu novo salario com 15% de aumento será de R${salario + aumento:.2f}')
---
+++ Mundo 1 [40 horas]/ex14.py
@@ -0,0 +1,3 @@
+grau_c = float(input('Digite um grau celsius: '))
+grau_f = (grau_c * 9/5) + 32
+print(f'Convertendo o Grau celsius {grau_c}ºC em Fahrenheit é {grau_f}°F')
