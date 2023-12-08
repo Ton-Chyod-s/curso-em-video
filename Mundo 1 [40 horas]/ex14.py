280e158f9eccc8d63321fa071eed2d590ab321c4
Klay <arqkdias@gmail.com>
2023-12-08 14:11:56 -0400

    doc: att

--- Mundo 1 [40 horas]/ex14.py
+++ Mundo 1 [40 horas]/ex14.py
@@ -1,3 +1,3 @@
-grau_c = float(input('Digite um grau celsius: '))
+grau_c = float(input('Digite um grau ºC: '))
 grau_f = (grau_c * 9/5) + 32
-print(f'Convertendo o Grau celsius {grau_c}ºC em Fahrenheit é {grau_f}°F')
+print(f'Convertendo o Grau celsius {grau_c}ºC em Fahrenheit é {grau_f}°F!')
