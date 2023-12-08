ff761c9a709f6b34efbd388d5632dbe6bfebb5fa
Klay <arqkdias@gmail.com>
2023-12-08 10:33:05 -0400

    doc: att

---
+++ Mundo 1 [40 horas]/ex8.py
@@ -0,0 +1,5 @@
+num = float(input('Digite um valor em metros: '))
+centimetros = num * 100
+milimetros = num * 1000
+
+print(f'{num} em centimetros é {centimetros} em milimetro é {milimetros}')
---
+++ Mundo 1 [40 horas]/ex9.py
@@ -0,0 +1,5 @@
+num = int(input('Digite um numero para ver sua tabuada: '))
+
+print('-' * 12)
+print(f'{num} * 0 = {num * 0}\n{num} * 1 = {num * 1}\n{num} * 2 = {num*2}\n{num} * 3 = {num*3}\n{num} * 4 = {num*4}\n{num} * 5 = {num*5}\n{num} * 6 = {num*6}\n{num} * 7 = {num*7}\n{num} * 8 = {num*8}\n{num} * 9 = {num*9}\n{num} * 10 = {num*10}')
+print('-' * 12)
