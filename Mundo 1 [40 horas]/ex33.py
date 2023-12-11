6c571fd7617df3fadbd5311dcaabc46627908e77
Klay <arqkdias@gmail.com>
2023-12-11 15:29:07 -0400

    doc: att

---
+++ Mundo 1 [40 horas]/ex33.py
@@ -0,0 +1,7 @@
+lista = [
+int(input('Digite um número: ')),
+int(input('Digite outro número: ')),
+int(input('Digite mais um número: '))
+]
+print(f'O menor valor digitado foi {min(lista)}')
+print(f'O maior valor digitado foi {max(lista)}')
