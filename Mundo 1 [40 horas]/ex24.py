24124e061634a82daf24be5c1659ce7e3843f1f7
Klay <arqkdias@gmail.com>
2023-12-11 10:33:27 -0400

    doc: att

--- Mundo 1 [40 horas]/ex24.py
+++ Mundo 1 [40 horas]/ex24.py
@@ -1,6 +1,4 @@
 cidade = str(input('Em que cidade você nasceu? ')).strip().upper()
 cidade = cidade.split()
-if cidade[0] == 'SANTO':
-    print('True')
-else:
-    print('False')
+print(cidade[0] == 'SANTO')
+    
