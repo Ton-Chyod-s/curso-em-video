b5ba229bcd161f32ed75489cdbaeabfdb922e646
Klay <arqkdias@gmail.com>
2023-12-11 16:10:46 -0400

    doc: att

---
+++ Mundo 1 [40 horas]/ex35.py
@@ -0,0 +1,15 @@
+print('-=' * 23)
+print('\033[32mAnalisador de Triângulo\033[m')
+print('-=' * 23)
+a = float(input('Digite primeiro segmento: '))
+b = float(input('Digite segundo segmento: '))
+c = float(input('Digite terceiro segmento: '))
+
+cond1 = (b - c) < a < b + c
+cond2 = (a - c) < b < a + c
+cond3 = (a - b) < c < a + b
+if cond1 and cond2 and cond3:
+    res = 'PODEM FORMAR'
+else:
+    res = 'NÃO PODEM FORMAR'
+print(f'Os segmentos acima {res} Triângulo!')
