1a31205733caac7ffb352391cb878687d74fb1cc
Klay <arqkdias@gmail.com>
2024-01-25 17:10:30 -0400

    Update script.js

--- JavaScript/Modulo D/ex015/script.js
+++ JavaScript/Modulo D/ex015/script.js
@@ -1,8 +1,25 @@
 function verificar() {
     var data = new Date()
     var anoAtual = data.getFullYear()
+    var txtIdade = document.getElementById('txt1')
+    var valorIdade = Number(txtIdade.value)
+    var resposta = document.getElementById('res')
+    var masculino = document.getElementById('masc')
+    var idade = anoAtual - valorIdade 
+    if (valorIdade < anoAtual) {
+        if (idade >= 1 && idade < 6) {
+            alert(idade)
 
+        } else if (idade >= 16 && idade < 18) {
+            alert(`${idade} adolescente`)
 
-    
-    alert("você clicou!!")
+        } else if (idade >= 18 && idade < 65) {
+            alert(`${idade} adulto`)
+            
+        } else {
+            alerto(`${idade} idoso`)
+        }
+    } else {
+        alert(`deu algo errado ae`)
+    }
 }
