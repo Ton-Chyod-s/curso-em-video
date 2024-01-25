2d81d0b6b978d71589b7c439d963cfa938cd4c6a
Klay <arqkdias@gmail.com>
2024-01-25 13:55:28 -0400

    Update ex013.js

---
+++ JavaScript/Mudolu D/ex013.js
@@ -0,0 +1,28 @@
+var agora = new Date()
+var diaSemana = agora.getDay()
+
+switch (diaSemana) {
+    case 0:
+        console.log('Domingo')
+    case 1:
+        console.log('Segunda-feira')
+        break
+    case 2:
+        console.log('Terça-feira')
+        break
+    case 3:
+        console.log('Quarta-feita')
+        break
+    case 4:
+        console.log('Quinta-feira')
+        break
+    case 5:
+        console.log('Sexta-feira')
+        break
+    case 6:
+        console.log('Sabado')
+        break
+
+}
+
+console.log(diaSemana)
