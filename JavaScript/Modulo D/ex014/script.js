549a8b23c6e20b5e24ea6f055751fb2a151afcad
Klay <arqkdias@gmail.com>
2024-01-25 15:32:17 -0400

    doc: att

--- JavaScript/Modulo D/ex014/script.js
+++ JavaScript/Modulo D/ex014/script.js
@@ -1,13 +1,20 @@
-var agora = new Date()
-var hora = agora.getHours()
-var p01 = document.getElementById('pg01') 
-
-p01.innerText = `Agora são exatamente ${hora} Horas!`
-
-if (hora < 12) {
-    document.body.style = 'red'
-} else if (hora >= 18) {
-    document.body.style = 'back'
-} else {
+function carregar() {
+    var agora = new Date()
+    var hora = agora.getHours()
+    var p01 = document.getElementById('pg01') 
+    var img = window.document.getElementById('imagem')
     
+    p01.innerText = `Agora são exatamente ${hora} Horas!`
+    
+    if (hora >=0 && hora < 12) {
+        document.body.style.background = '#BD7E8D'
+        img.src = 'manhã.jpg'
+    } else if (hora >= 12 && hora < 18) {
+        document.body.style.background = '#592D11'
+        img.src = 'tarde.jpg'
+    } else {
+        img.src = 'noite.jpg'
+        document.body.style.background = '#362943'
+    }
 }
+
