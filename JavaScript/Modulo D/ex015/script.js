c88c70ae1757fe5748944688f6f11bae55d4fd1e
Klay <arqkdias@gmail.com>
2024-01-26 09:31:27 -0400

    doc: att

--- JavaScript/Modulo D/ex015/script.js
+++ JavaScript/Modulo D/ex015/script.js
@@ -11,24 +11,49 @@ function verificar() {
         var radioSex = document.getElementsByName('radsex')
         var idade = anoAtual - valorIdade
         var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id','foto')
 
         if (radioSex[0].checked) {
-            genero = 'Homen'
-            if (idade >= 1 || idade < 6) {
-            } else if (idade >= 16 || idade < 18) {
-                alert(`${idade} adolescente`)
-    
-            } else if (idade >= 18 || idade < 65) {
-                alert(`${idade} adulto`)
+            genero = 'Homem'
+            
+            if (idade >= 0 && idade < 10) {
+                img.setAttribute('src','bebe.jpg')
+                alert(`${idade} criança`)
+                document.body.style.background = 'red'
                 
+            } else if (idade < 21) {
+                alert(`${idade} adolescente`)
+                img.setAttribute('src','adolescente homen.jpg')
+    
+            } else if (idade < 50) {
+                alert(`${idade} adulto`)
+                img.setAttribute('src','homem.jpg')
             } else {
                 alert(`${idade} idoso`)
+                img.setAttribute('src','senhor.jpg')
             }
         } else if (radioSex[1].checked) {
             genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                alert(`${idade} criança`)
+                img.setAttribute('src','bebe.jpg')
 
-        } 
-        resposta.innerText = `detectamos ${genero} de ${idade}`
-        
+            } else if (idade < 21) {
+                alert(`${idade} adolescente`)
+                img.setAttribute('src','adolescente mulher.jpg')
+    
+            } else if (idade < 50) {
+                alert(`${idade} adulto`)
+                img.setAttribute('src','mulher.jpg')
+                
+            } else {
+                alert(`${idade} idoso`)
+                img.setAttribute('src','senhora.jpg')
+            }
+        }
+        resposta.style.textAlign = 'center'
+        resposta.innerHTML = `detectamos ${genero} de ${idade} anos.`
+        resposta.appendChild(img)
     }
     }
